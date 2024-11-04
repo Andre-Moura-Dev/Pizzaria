2c3d1541c6291f0786fc25b7a01f309280d4dec9
Andre-Moura-Dev <andreoliveiramoura06@gmail.com>
2024-11-03 22:16:53 -0300

    Atualizações em arquivos

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,28 @@
-//Fechar e abrir barra de navegação
-
-const toggleButton = document.getElementById('toggleSidebar');
-const sidebar = document.querySelector('.side-bar');
-
-toggleButton.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
-});
-
 //Alternar Tema Claro e Escuro
-
 const themeButton = document.getElementById('toggleTheme');
 
 themeButton.addEventListener('click', () => {
     document.body.classList.toggle('dark');
-    sidebar.classList.toggle('dark');
     document.querySelector('header').classList.toggle('dark');
     document.querySelector('footer').classList.toggle('dark');
+});
+
+//Abrir e fechar as informações dos cards
+document.addEventListener('DOMContentLoaded', function() {
+    // Seleciona todos os botões de toggle
+    const toggleButtons = document.querySelectorAll('.toggle-content');
+
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Seleciona o conteúdo extra associado ao botão clicado
+            const extraContent = button.nextElementSibling;
+
+            // Alterna a visibilidade
+            if (extraContent.style.display === 'none' || extraContent.style.display === '') {
+                extraContent.style.display = 'block';
+            } else {
+                extraContent.style.display = 'none';
+            }
+        });
+    });
 });
